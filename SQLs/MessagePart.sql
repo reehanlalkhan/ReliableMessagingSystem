5c88ff3a7dab8d4f4c9af539c471109cce6a28c0
Reehan <reehan.lalkhan@gmail.com>
2020-10-15 13:25:56 +0530

    Adding message part delivery status storage

--- SQLs/MessagePart.sql
+++ SQLs/MessagePart.sql
@@ -13,7 +13,9 @@ CREATE TABLE `message_part` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `start_offset` int(11) UNSIGNED NOT NULL,
   `end_offset` int(11) UNSIGNED NOT NULL,
+  `message_part` varchar(150) NOT NULL,
   `is_last` TINYINT(1) NOT NULL DEFAULT '0',
+  `is_delivered` TINYINT(1) NOT NULL DEFAULT '0',
   `message_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `FK_part_in_message` FOREIGN KEY (`message_id`) REFERENCES `message` (`id`)
