384f6e2aba4709de8b96abd15a26046edd6098d7
Reehan <reehan.lalkhan@gmail.com>
2020-10-15 11:49:31 +0530

    API to get current status of message

--- MessagingCore/src/main/java/in/novopay/MessagingCore/api/MessageServiceApiResource.java
+++ MessagingCore/src/main/java/in/novopay/MessagingCore/api/MessageServiceApiResource.java
@@ -4,13 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.novopay.MessagingCore.constants.MessageApiConstants;
+import in.novopay.MessagingCore.constants.MessageResponse;
 import in.novopay.MessagingCore.data.dto.MessageData;
+import in.novopay.MessagingCore.exception.InvalidMessageId;
 import in.novopay.MessagingCore.service.MessagePlatformService;
 
 @RestController
@@ -24,16 +28,23 @@ public class MessageServiceApiResource {
 		this.messageService = messageService;
 	}
 
-	@GetMapping
-	public ResponseEntity<String> getAllMessage() {
-		return new ResponseEntity<String>("Response", HttpStatus.OK);
+	@GetMapping("/{messageId}")
+	public ResponseEntity<MessageResponse> getMessageStatus(@PathVariable long messageId) throws InvalidMessageId {
+		MessageResponse response = messageService.getMessageStatus(messageId);
+		return new ResponseEntity<MessageResponse>(response, HttpStatus.OK);
 
 	}
 
 	@PostMapping("/sendMessage")
-	public ResponseEntity sendMessage(@RequestBody final MessageData messageData) {
-		String output = "";
-		return new ResponseEntity("output", HttpStatus.OK);
+	public ResponseEntity<MessageResponse> sendMessage(@RequestBody final MessageData messageData) {
+		MessageResponse response = messageService.sendMessage(messageData);
+		return new ResponseEntity<MessageResponse>(response, HttpStatus.OK);
+	}
+
+	@PutMapping("/{messageId}/{partId}")
+	public ResponseEntity acknowledgeMessage(@PathVariable long messageId, @PathVariable long partId) {
+		MessageResponse response = MessageResponse.MSG_DEFAULT_CODE;
+		return new ResponseEntity(response.getResponse(), HttpStatus.OK);
 	}
 
 }
--- MessagingCore/src/main/java/in/novopay/MessagingCore/service/MessagePlatformServiceImpl.java
+++ MessagingCore/src/main/java/in/novopay/MessagingCore/service/MessagePlatformServiceImpl.java
@@ -1,31 +1,46 @@
 package in.novopay.MessagingCore.service;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import in.novopay.MessagingCore.constants.MessageResponse;
 import in.novopay.MessagingCore.data.dao.MessageDataDao;
 import in.novopay.MessagingCore.data.db.Message;
 import in.novopay.MessagingCore.data.dto.MessageData;
+import in.novopay.MessagingCore.exception.InvalidMessageId;
 
 @Service
 public class MessagePlatformServiceImpl implements MessagePlatformService {
 
 	private final static Logger logger = LoggerFactory.getLogger(MessagePlatformServiceImpl.class);
-	
+
 	@Autowired
 	MessageDataDao messageDao;
 
 	@Override
-	public String sendMessage(MessageData messageData) {
+	public MessageResponse sendMessage(MessageData messageData) {
 		logger.debug("Data:" + messageData);
 		Message m = new Message();
 		m.setMessage(messageData.getMessage());
 		m.setSender(messageData.getSender());
 		m.setRecipient(messageData.getRecipient());
 		messageDao.saveAndFlush(m);
-		return "";
+		return MessageResponse.MSG_SUBMITTED_CODE;
+	}
+
+	@Override
+	public MessageResponse getMessageStatus(long messageId) throws InvalidMessageId {
+		try {
+			Message m = messageDao.getOne(messageId);
+			int status = m.getStatus();
+			return MessageResponse.getResponseCode(status);
+		} catch (EntityNotFoundException exp) {
+			throw new InvalidMessageId(messageId);
+		}
 	}
 
 }
